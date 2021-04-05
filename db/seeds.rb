464b56323a62296aa6dcd19681e64be70fc23d58
palepanorama <matthew.keith.thomas@gmail.com>
2021-04-05 12:23:50 -0500

    adds seed data

--- db/seeds.rb
+++ db/seeds.rb
@@ -2,7 +2,6 @@
 # The data can then be loaded with the rails db:seed command (or created alongside the database with db:setup).
 #
 # Examples:
-
 #   movies = Movie.create([{ name: 'Star Wars' }, { name: 'Lord of the Rings' }])
 #   Character.create(name: 'Luke', movie: movies.first)
 Attorney.create(name: "Name 1", location: "Location 1", description: "Description 1")
