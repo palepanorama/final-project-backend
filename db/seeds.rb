9e4120a1469f05c5e35317c2637f9c226845e76e
palepanorama <matthew.keith.thomas@gmail.com>
2021-05-04 09:35:23 -0500

    init commit

--- db/seeds.rb
+++ db/seeds.rb
@@ -2,9 +2,10 @@
 # The data can then be loaded with the rails db:seed command (or created alongside the database with db:setup).
 #
 # Examples:
+#
 #   movies = Movie.create([{ name: 'Star Wars' }, { name: 'Lord of the Rings' }])
 #   Character.create(name: 'Luke', movie: movies.first)
-Attorney.create(name: "Name 1", location: "Location 1", description: "Description 1")
-Attorney.create(name: "Name 2", location: "Location 2", description: "Description 2")
-Attorney.create(name: "Name 3", location: "Location 3", description: "Description 3")
-Attorney.create(name: "Name 4", location: "Location 4", description: "Description 4")
+# Attorney.create(name: "Name 1", location: "Location 1", description: "Description 1")
+# Attorney.create(name: "Name 2", location: "Location 2", description: "Description 2")
+# Attorney.create(name: "Name 3", location: "Location 3", description: "Description 3")
+# Attorney.create(name: "Name 4", location: "Location 4", description: "Description 4")
